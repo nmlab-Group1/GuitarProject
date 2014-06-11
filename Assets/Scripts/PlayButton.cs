9a0bf23be7b49258ce973ab2f1045eba6a99c0e4
Roger-Wu <gsetcrw@gmail.com>
2014-06-12 00:07:30 +0800

    Initialization

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton () {
+		Application.LoadLevel ("GameScene");
+		//AutoFade.LoadLevel("GameScene", 3, 1, Color.white);
+	}
+}
